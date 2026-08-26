c1473330fe1cf3c61966bbbf1813ef1a22070e45
MarcelloKP <marcelloputro@gmail.com>
2026-08-26 17:17:32 +0700

    Initial Commit

---
+++ cello_project1/src/main/java/com/mycompany/cello_project1/Cello_project1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cello_project1;
+
+public class Cello_project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Marcello Krisdaryanto Putro");
+        System.out.println("NPM : 2505060021");
+        System.out.println("Alamat : Surakarta");
+        System.out.println("No_Hp : 08946462531");
+         
+        
+        
+    }
+}
